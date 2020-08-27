69bce5ae9d83967b595e435337647ab6d12e0d1a
sharatramaraju <sharat1000@gmail.com>
2020-08-27 11:37:03 -0500

    forms

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ myBtn1.addEventListener("click", clickHandler);
 
 const myBtn2 = document.getElementById("btn2");
 myBtn2.addEventListener("click", clickHandler);
+myBtn2.addEventListener("click", clickHandler2);
 
 function clickHandler(event) {
   console.log(event);
@@ -15,4 +16,19 @@ function clickHandler(event) {
   if (event.target.id === "btn2") {
     console.log("Handling button 2 case");
   }
+}
+
+function clickHandler2(event) {
+  console.log("clickHandler2");
+  console.log(event);
+  console.log(event.target.id);
+}
+
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  console.log("Form was submitted");
+  console.log(document.getElementById("fname").value);
 }
